f6f2575141c70a98b0ba8b333eaa37ffa1c244ba
daniellelewis14 <yurts.third_0v@icloud.com>
2023-07-10 01:26:12 -0400

    Add program functionality and initial UI

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,14 +8,149 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
+    @State private var userScore = 0
+    
+    
+    
+    func startGame() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                
+                let allWords = startWords.components(separatedBy: "\n")
+                
+                rootWord = allWords.randomElement() ?? "silkworm"
+                
+                return
+            }
+            
+            fatalError("Could not load start.txt from bundle.")
         }
-        .padding()
+    }
+    
+    
+    
+    
+    func addNewWord() {
+        
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespaces)
+        
+        guard answer.count > 0 else { return }
+        
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original")
+            return
+        }
+        
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+            return
+        }
+        
+        guard isReal(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
+        
+        guard longerThanThree(word: answer) else {
+            wordError(title: "Word too short", message: "Word can't be shorter than 3 letters")
+            return
+        }
+        
+        
+        
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+        }
+        userScore += newWord.count
+        newWord = ""
+    }
+    
+    
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    
+    
+    func isPossible(word: String) -> Bool {
+        
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
+    }
+    
+    func longerThanThree(word: String) -> Bool {
+        if (word.count >= 3) {
+            return true
+        }
+        return false
+    }
+    
+    
+    var body: some View {
+        NavigationView {
+            List {
+                
+                Section("Score: \(userScore)") {
+                    TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                }
+            
+                
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .toolbar {
+                Button("New Word", action: startGame)
+            }
+        }
+        .onAppear(perform: startGame)
+        .onSubmit(addNewWord)
+        .alert(errorTitle, isPresented: $showingError) {
+            Button("OK", role: .cancel) { }
+        } message: {
+            Text(errorMessage)
+        }
     }
 }
 
